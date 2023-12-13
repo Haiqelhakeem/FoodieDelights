f54ef15a82e15fac937b41eca4cc85aacfa10351
Haiqelhakeem <haiqelhakeem@gmail.com>
2023-12-13 09:50:16 +0700

    [connection db]

--- src/services/place.service.jsx
+++ src/services/place.service.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getPlaces = async (callback) => {
     await axios
-      .get("http://127.0.0.1:3000/places", {
+      .get("https://foodie-delights-api.vercel.app/places", {
         headers: {
           "Content-Type": "application/json",
         },
